e4329d7b10cf0a5a819d51c263be2ef2c1f3632f
Алексей <malyshevalexey96@gmail.com>
2025-04-11 17:00:31 +0300

    Web-manifest fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/PWA-test/',
+    base: '/',
     plugins: [
         vue(),
         vueDevTools(),
